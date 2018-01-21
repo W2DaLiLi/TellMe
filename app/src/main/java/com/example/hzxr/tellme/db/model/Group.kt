bd0040ae1ed733cad557294a22942c572bec98e0
Hzxr_ <13277250082@163.com>
2018-01-21 16:35:19 +0800

    feature: define group model

--- app/src/main/java/com/example/hzxr/tellme/db/model/Group.kt
+++ app/src/main/java/com/example/hzxr/tellme/db/model/Group.kt
@@ -1,7 +1,17 @@
 package com.example.hzxr.tellme.db.model
 
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import io.objectbox.relation.ToMany
+
 /**
  * Created by Hzxr on 2018/1/20.
  */
-class Group {
-}
+@Entity
+data class Group (
+        @Id
+        var name: String,
+        var description: String? = null,
+        var admin: String,
+        var members : ToMany<Member>
+)
