14a087b38fa6c5797cb82fd85d3c226f9fd7ed4f
Hzxr_ <13277250082@163.com>
2018-01-21 16:30:14 +0800

    feature: define Msg model

--- app/src/main/java/com/example/hzxr/tellme/db/model/Msg.kt
+++ app/src/main/java/com/example/hzxr/tellme/db/model/Msg.kt
@@ -1,7 +1,18 @@
 package com.example.hzxr.tellme.db.model
 
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+
 /**
  * Created by Hzxr on 2018/1/20.
  */
-class Msg {
-}
+@Entity
+data class Msg(
+        @Id
+        var id: Long,
+        var type: String,
+        var to: String,
+        var from: String,
+        var content: String,
+        var data: String
+)
