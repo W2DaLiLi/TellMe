2943d0d097967b3d46e32ebd985ddbf018ea203c
Hzxr_ <13277250082@163.com>
2018-01-21 16:40:47 +0800

    feature: define session model

--- app/src/main/java/com/example/hzxr/tellme/db/model/Session.kt
+++ app/src/main/java/com/example/hzxr/tellme/db/model/Session.kt
@@ -1,7 +1,18 @@
 package com.example.hzxr.tellme.db.model
 
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import io.objectbox.relation.ToMany
+import io.objectbox.relation.ToOne
+
 /**
  * Created by Hzxr on 2018/1/20.
  */
-class Session {
-}
+@Entity
+data class Session(
+        @Id
+        var sessionName: String,
+        var updateData: String,
+        var lastMsg: ToOne<Msg>,
+        var members: ToMany<Member>
+)
