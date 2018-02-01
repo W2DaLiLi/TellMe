d5d8d93a1e8958ec7bee1e5e5ed622b2cc6a3ced
Hzxr_ <13277250082@163.com>
2018-02-01 14:27:02 +0800

    feature:Login UI

--- app/src/main/java/com/example/hzxr/tellme/UI/VM/LoginViewModel.kt
+++ app/src/main/java/com/example/hzxr/tellme/UI/VM/LoginViewModel.kt
@@ -2,11 +2,28 @@ package com.example.hzxr.tellme.UI.VM
 
 import android.app.Activity
 import android.databinding.BaseObservable
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.example.hzxr.tellme.databinding.ActivityLoginBinding
 
 /**
  * Created by Hzxr on 2018/1/20.
  */
 class LoginViewModel(activity: Activity, binding: ActivityLoginBinding) : BaseViewModel<ActivityLoginBinding>(activity, binding) {
-    var 
+
+    var username: String? = null
+    var password: String? = null
+
+    var rememberPW: Boolean = false
+    var autoLogin: Boolean = false
+
+    val onLoginButtonClickListener: View.OnClickListener
+        get() = View.OnClickListener { login() }
+
+    private fun login() {
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) return
+        Log.d("TAG","username:" + username + " password:" + password + " remember:" + rememberPW + " auto:" + autoLogin)
+    }
 }
