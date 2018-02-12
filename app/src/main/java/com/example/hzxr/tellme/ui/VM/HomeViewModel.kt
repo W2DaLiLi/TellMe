5fc8921e13feaaa5c0c8d2448c07673ff0ae257c
Hzxr_ <13277250082@163.com>
2018-02-12 13:03:45 +0800

    feature:fix home-menu

--- app/src/main/java/com/example/hzxr/tellme/ui/VM/HomeViewModel.kt
+++ app/src/main/java/com/example/hzxr/tellme/ui/VM/HomeViewModel.kt
@@ -13,27 +13,6 @@ import com.example.hzxr.tellme.databinding.ActivityHomeBinding
  */
 class HomeViewModel(activity: Activity, binding: ActivityHomeBinding) : BaseViewModel<ActivityHomeBinding>(activity, binding) {
 
-    val onMenuItemClickListener: Toolbar.OnMenuItemClickListener
-        get() = Toolbar.OnMenuItemClickListener { item ->
-            if (item.itemId == R.id.action_settings) {
-                Log.d("TAG", "test setting button")
-            }
-            true
-        }
-// 在使用databinding binding 这个监听的时候总是报错，类型问题，目前没有找到好的解决办法，先暂时这样写。
-//    val onNavigationItemSelectedListener: NavigationView.OnNavigationItemSelectedListener
-//        get() = NavigationView.OnNavigationItemSelectedListener {
-//            item ->
-//            when(item.itemId) {
-//                R.id.navigation_profile -> {
-//                    Log.d("TAG", "navigation profile")
-//                }
-//                R.id.navigation_settings -> {
-//                    Log.d("TAG", "navigation settings")
-//                }
-//            }
-//            true
-//        }
     init {
         binding.navigationView.setNavigationItemSelectedListener {
             item ->
@@ -48,4 +27,28 @@ class HomeViewModel(activity: Activity, binding: ActivityHomeBinding) : BaseView
             true
         }
     }
+
+    val onMenuItemClickListener: Toolbar.OnMenuItemClickListener
+        get() = Toolbar.OnMenuItemClickListener { item ->
+            if (item.itemId == R.id.action_settings) {
+                Log.d("TAG", "test setting button")
+            }
+            true
+        }
+
+// 在使用databinding binding 这个监听的时候总是报错，类型问题，目前没有找到好的解决办法，先暂时这样写。
+//    val onNavigationItemSelectedListener: NavigationView.OnNavigationItemSelectedListener
+//        get() = NavigationView.OnNavigationItemSelectedListener {
+//            item ->
+//            when(item.itemId) {
+//                R.id.navigation_profile -> {
+//                    Log.d("TAG", "navigation profile")
+//                }
+//                R.id.navigation_settings -> {
+//                    Log.d("TAG", "navigation settings")
+//                }
+//            }
+//            true
+//        }
+
 }
