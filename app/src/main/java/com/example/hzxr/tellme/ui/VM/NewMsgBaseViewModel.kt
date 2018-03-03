d0bc2beb90b56ba13563ecafcbb3454ba97dbd2a
Hzxr_ <13277250082@163.com>
2018-03-03 19:57:39 +0800

    fix: init Message Model

---
+++ app/src/main/java/com/example/hzxr/tellme/ui/VM/NewMsgBaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.hzxr.tellme.ui.VM
+
+import android.databinding.BaseObservable
+
+/**
+ * Created by Hzxr on 2018/3/3.
+ */
+abstract class NewMsgBaseViewModel : BaseObservable(){
+}
---
+++ app/src/main/java/com/example/hzxr/tellme/ui/VM/NewMsgNormalViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.hzxr.tellme.ui.VM
+
+/**
+ * Created by Hzxr on 2018/3/3.
+ */
+class NewMsgNormalViewModel: NewMsgBaseViewModel() {
+}
